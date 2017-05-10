2287d4b648df9281d5cbf039f00775ec0c19549d
Felipe Faria <felipe.faria@farfetch.com>
2017-05-10 14:43:48 +0100

    TESTE

--- testGitStrategies/Program.cs
+++ testGitStrategies/Program.cs
@@ -6,7 +6,7 @@ namespace testGitStrategies
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! UPDATE");
         }
     }
 }
